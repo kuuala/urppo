cb0e57eaae0c0ad8fff9c5c09b5ce5be869c86a3
kuuala <kumaritof@gmail.com>
2025-05-08 14:07:49 +0300

    change main.py to get weather
    main.py now gets weather in Tomsk

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import requests
 
+def weather_api(city):
+    try:
+        response = requests.get(f"https://wttr.in/{city}?format=1")
+        if response.status_code == 200:
+            weather_data = response.text
+            return weather_data
+        else:
+            print(f"Error: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"Error while getting weather: {e}")
+        return None
+    
+    
 def main():
-    response = requests.get("https://httpbin.org/get", params={"test_phrase": "Hello, World!"})
-    data = response.json()
-    print(data['args']['test_phrase'])
+    weather_api_response = weather_api("Tomsk")
+    if weather_api_response:
+        print(f"Weather in Tomsk: {weather_api_response}")
+    else:
+        print("Failed to retrieve weather data")
     
 if __name__ == "__main__":
     main()
